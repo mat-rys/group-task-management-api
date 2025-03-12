e8c65d6e2c592426788cf1404f8ae3376b3657dc
Mateusz <matrys000@utp.edu.pl>
2025-03-12 15:49:09 +0100

    TaskHub V.1.8 Comments a few more controller tests

--- TaskHub.Tests/CommentsTest.cs
+++ TaskHub.Tests/CommentsTest.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TaskHub.Controllers;
 using TaskHub.Entities;
+using TaskHub.Models;
 using TaskHub.Services;
 
 namespace TaskHub.Tests
@@ -70,6 +71,56 @@ namespace TaskHub.Tests
             var okResult = Assert.IsType<OkObjectResult>(resultListComments);
             var resultComment = Assert.IsType<List<TaskComment>>(okResult.Value);
             Assert.Equal(2, resultComment.Count());
+        }
+
+        [Fact]
+        public async Task UpdateComment_ReturnOkObjectAndComment_WhenContentInCommentChanged()
+        {
+            //Arrange
+            int taskId = 1;
+            TaskCommentDto contentSendedToUpdate = new() {Content = "after"};
+            TaskComment taskCommentReturned = new() { Id = 1, Content = "after", CreatedAt= DateTime.Now};
+
+            _taskCommentService.Setup(svc => svc.UpdateComment(taskId, contentSendedToUpdate))
+               .ReturnsAsync(taskCommentReturned);
+            
+            //Act
+            var result = await _controller.UpdateTContent(taskId, contentSendedToUpdate);
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedComment = Assert.IsType<TaskComment>(okResult.Value);
+
+            Assert.Equal(contentSendedToUpdate.Content, returnedComment.Content);
+        }
+
+        [Fact]
+        public async Task PostNewCommentForTask_ReturnCreatedAtAction_WhenCommentCreated()
+        {
+            TaskCommentDto contentSendedToUpdate = new() { Content = "NewComment" };
+            TaskComment taskComment = new() {
+                Id = 1,
+                Content = contentSendedToUpdate.Content,
+                CreatedAt = DateTime.Now,
+                TaskTodo = new() { Id = 1, Description = "task"},
+                UserProfile = new() { UserId = "aaa-222-111", Role = "LEADER", UserName = "mmm" }
+            };
+
+            _taskCommentService.Setup(svc => svc.PostNewComment(taskComment.TaskTodo.Id,
+                taskComment.UserProfile.UserId,
+                contentSendedToUpdate))
+                .ReturnsAsync(taskComment);
+
+            //Act
+            var result = await _controller.PostNewCommentForTask(taskComment.TaskTodo.Id,
+                taskComment.UserProfile.UserId,
+                contentSendedToUpdate);
+
+            //Assert
+            var okResult = Assert.IsType<CreatedAtActionResult>(result);
+            var returnedComment = Assert.IsType<TaskComment>(okResult.Value);
+
+            Assert.Equal(contentSendedToUpdate.Content, returnedComment.Content);
         }
     }
 }
